5ff4d8c08169af1d327bf225ff75014b664715de
sindetisarah <sindetisara@gmail.com>
2021-06-14 07:30:20 +0300

    Recycler view of Fibonacci numbers

---
+++ app/src/main/java/com/example/numbersrecyclerview/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.numbersrecyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.math.BigInteger
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var rvNumbers=findViewById<RecyclerView>(R.id.rvNumbers)
+
+        rvNumbers.layoutManager=
+            LinearLayoutManager(baseContext,)
+        var numbersAdapter=NumbersRecyclerViewAdapter(fibonacci(maxNumber=100))
+        rvNumbers.adapter=numbersAdapter
+    }
+}
+fun fibonacci(maxNumber:Int):MutableList<BigInteger>{
+    var count=0;
+    var firstNumber=0.toBigInteger()
+    var secondNumber=1.toBigInteger()
+    var sum = 0.toBigInteger()
+    var fibNumbers= mutableListOf<BigInteger>()
+
+    while(count<=maxNumber){
+        println(firstNumber)
+        sum = firstNumber + secondNumber
+        firstNumber = secondNumber
+        secondNumber = sum
+        count ++
+        fibNumbers.add(firstNumber)
+    }
+    return fibNumbers
+
+
+}
+
---
+++ app/src/main/java/com/example/numbersrecyclerview/NumbersRecyclerViewAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.numbersrecyclerview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import java.math.BigInteger
+
+class NumbersRecyclerViewAdapter(var numbersList:MutableList<BigInteger>):RecyclerView.Adapter<NumbersViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumbersViewHolder {
+        val itemView=
+            LayoutInflater.from(parent.context).inflate(R.layout.number_list_item,parent,false)
+        return NumbersViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: NumbersViewHolder, position: Int) {
+        holder.tvNumber.text=numbersList.get(position).toString()
+
+    }
+
+    override fun getItemCount(): Int {
+        return numbersList.size
+    }
+}
+class NumbersViewHolder(val itemView: View): RecyclerView.ViewHolder(itemView){
+    val tvNumber=itemView.findViewById<TextView>(R.id.tvNumbers)
+
+}
